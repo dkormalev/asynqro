91e8a7832ff409433c4f70faaa8fcbf69cf1c62a
Denis Kormalev <denis.kormalev@opensoftdev.com>
2019-07-18 14:42:54 -0700

    recipe only upload on test conan build

--- .ci/conan/build.py
+++ .ci/conan/build.py
@@ -1,7 +1,7 @@
 from cpt.packager import ConanMultiPackager
 
 def test_build():
-    builder = ConanMultiPackager(channel="build-testing", upload_only_when_stable=True)
+    builder = ConanMultiPackager(channel="build-testing", upload_only_when_stable=True, upload_only_recipe=True)
     builder.add({"arch": "x86_64", "build_type": "Debug"}, {"asynqro:build_tests": True})
     builder.run()
 
