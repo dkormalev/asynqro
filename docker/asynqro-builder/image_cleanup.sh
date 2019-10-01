eefb44cdb4df6bfec0e9049775fd31ce8168975e
Dennis Kormalev <kormalev.denis@gmail.com>
2019-10-01 19:43:01 -0400

    Compatibility check for older compilers: gcc7.4 and clang 6.0

--- docker/asynqro-builder/image_cleanup.sh
+++ docker/asynqro-builder/image_cleanup.sh
@@ -2,10 +2,10 @@
 
 set -e;
 
-rm -rf /var/lib/apt/lists/*;
-find /usr/share/locale -name '??' -o -name '??_??' | xargs rm -rf;
-find /usr/share/doc -type f -a ! -iname 'copyright*' -delete;
-find /usr/share/qt5/doc -type f -a ! -iname 'copyright*' -delete;
-rm -rf /var/tmp/*;
-rm -rf /tmp/*;
-rm -rf /usr/share/man;
+rm -rf /var/lib/apt/lists/* || true;
+find /usr/share/locale -name '??' -o -name '??_??' | xargs rm -rf || true;
+find /usr/share/doc -type f -a ! -iname 'copyright*' -delete || true;
+find /usr/share/qt5/doc -type f -a ! -iname 'copyright*' -delete || true;
+rm -rf /var/tmp/* || true;
+rm -rf /tmp/* || true;
+rm -rf /usr/share/man || true;
