86ebc6ffc2d217267f443f36f2c369749d4d1f8a
Dennis Kormalev <kordenis@amazon.com>
2020-10-12 23:55:48 -0400

    No Qt dependency in tests stacktrace output

--- tests/impl/main.cpp
+++ tests/impl/main.cpp
@@ -1,11 +1,13 @@
-#include "gtest/gtest.h"
+#include "common/crash_handler.h"
 
+#include "gtest/gtest.h"
 #ifdef ASYNQRO_QT_SUPPORT
 #    include <QCoreApplication>
 #endif
 
 int main(int argc, char **argv)
 {
+    initCrashHandler();
 #ifdef ASYNQRO_QT_SUPPORT
     QCoreApplication a(argc, argv);
 #endif
